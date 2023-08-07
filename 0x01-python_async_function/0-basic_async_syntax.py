d9fbf8a1ddc9e095587f6a032c9ec6e2a8de8596
Toluthedev <enochjesse884@gmail.com>
2023-08-07 22:00:04 +0100

    added test case

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,12 +6,22 @@
 import asyncio
 import random
 
-
 async def wait_random(max_delay: int = 10) -> float:
     """
-    waits for a random delay between 0 and max_delay (included and float value)
-    seconds and eventually returns it.
+    Asynchronous coroutine that waits for a random delay between 0 and max_delay seconds
+    and eventually returns the delay.
+
+    Args:
+        max_delay (int): The maximum delay value in seconds. Default is 10.
+
+    Returns:
+        float: The random delay in seconds.
     """
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+    random_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(random_delay)
+    return random_delay
+
+# Test cases
+print(asyncio.run(wait_random()))
+print(asyncio.run(wait_random(5)))
+print(asyncio.run(wait_random(15)))
